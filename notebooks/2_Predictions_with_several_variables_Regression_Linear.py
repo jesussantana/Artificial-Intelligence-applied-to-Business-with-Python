9c5dff58fd0d5a7fe621287dfb56580d4bed4477
jesussantana <jesus@esmarketingdigital.com>
2021-05-27 16:30:29 +0200

    Predictions with several variables

--- notebooks/2_Predictions_with_several_variables_Regression_Linear.py
+++ notebooks/2_Predictions_with_several_variables_Regression_Linear.py
@@ -2,6 +2,7 @@
 # ==============================================================================
 
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import linear_model
 import statsmodels.api as sm
 
@@ -18,7 +19,12 @@ Stock_Market = {'Year': [2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,
 df = pd.DataFrame(Stock_Market,columns=['Year','Month','Interest_Rate','Unemployment_Rate','Stock_Index_Price']) 
 print (df)
 
-
+plt.scatter(df['Interest_Rate'], df['Stock_Index_Price'], color='red')
+plt.title('Stock Index Price Vs Interest Rate', fontsize=14)
+plt.xlabel('Interest Rate', fontsize=14)
+plt.ylabel('Stock Index Price', fontsize=14)
+plt.grid(True)
+plt.show()
 
 
 df = pd.DataFrame(Stock_Market,columns=['Year','Month','Interest_Rate','Unemployment_Rate','Stock_Index_Price'])
