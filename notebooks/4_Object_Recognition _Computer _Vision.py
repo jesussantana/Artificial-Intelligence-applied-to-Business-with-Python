653b65728c124fcb59827922c1ea0d2dfc994d79
jesussantana <jesus@esmarketingdigital.com>
2021-05-28 10:47:19 +0200

    Computer Vision

--- notebooks/4_Object_Recognition _Computer _Vision.py
+++ notebooks/4_Object_Recognition _Computer _Vision.py
@@ -2,10 +2,12 @@
 # ==============================================================================
 
 # io import BytesIO
+
 #from google.colab import files 
 #uploaded = files.upload()
-#from google.colab import files
+
 #from IPython.display import Image
+
 import cv2
 import matplotlib.pyplot as plt
 import cvlib as cv
@@ -15,7 +17,9 @@ from tensorflow.keras.layers import Input, Dense
 im = cv2.imread('../reports/figures/foto.jpg')
 bbox, label, conf = cv.detect_common_objects(im)
 output_image = draw_bbox(im, bbox, label, conf)
+
 plt.imshow(output_image)
 plt.show()
+
 print('Coches detectados: '+ str(label.count('car')))
 print('Personas detectadas: '+ str(label.count('car')))
